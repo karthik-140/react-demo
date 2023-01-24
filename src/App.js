6073c3ce15af6aef62f86eaee64a963e3a57ee8e
karthik-140 <shanigaramkarthik@gmail.com>
2023-01-24 22:47:08 +0530

    using-props-looping

--- src/App.js
+++ src/App.js
@@ -1,10 +1,50 @@
 import ExpenseItems from "./components/ExpenseItems";
 
 function App(){
+  const expenses = [
+    {
+      id: 'e1',
+      title: 'Toilet Paper',
+      amount: 94.12,
+      date: new Date(2020, 7, 14),
+      location:'Hyderabad',
+    },
+    { id: 'e2', title: 'New TV', amount: 799.49, date: new Date(2021, 2, 12), location:'warangal', },
+    {
+      id: 'e3',
+      title: 'Car Insurance',
+      amount: 294.67,
+      date: new Date(2021, 2, 28),
+      location:'Bangalore',
+    },
+    {
+      id: 'e4',
+      title: 'New Desk (Wooden)',
+      amount: 450,
+      date: new Date(2021, 5, 12),
+      location:'Chennai'
+    },
+    {
+      id:'e5',
+      title:'Mobile',
+      amount:2000,
+      date:new Date(2000,5,14),
+      location:'kazipet',
+    }
+  ];
   return (
     <div>
       <h2>Let's Get Started</h2>
-      <ExpenseItems></ExpenseItems>
+      {expenses.map(data => <ExpenseItems
+      title={data.title}
+      amount={data.amount}
+      date={data.date}
+      location={data.location}
+      ></ExpenseItems>)}
+      {/* <ExpenseItems title={expenses[0].title} amount={expenses[0].amount} date={expenses[0].date} location={expenses[0].location}></ExpenseItems>
+      <ExpenseItems title={expenses[1].title} amount={expenses[1].amount} date={expenses[1].date} location={expenses[1].location}></ExpenseItems>
+      <ExpenseItems title={expenses[2].title} amount={expenses[2].amount} date={expenses[2].date} location={expenses[2].location}></ExpenseItems>
+      <ExpenseItems title={expenses[3].title} amount={expenses[3].amount} date={expenses[3].date} location={expenses[3].location}></ExpenseItems> */}
     </div>
   )
 }
--- src/components/ExpenseItems.js
+++ src/components/ExpenseItems.js
@@ -1,18 +1,14 @@
 import "./ExpenseItems.css"
 
-function ExpenseItems(){
-    const ExpenseDate=new Date(2023,2,14);
-    const ExpenseItem='Car Insurance';
-    const ExpensePrice= 200;
-    const LocationOfExpenditure='Hyderabad';
+function ExpenseItems(props){
     return (
         <div>
             <div className="expense-item">
-                <div>{ExpenseDate.toISOString()}</div>
+                <div>{props.date.toISOString()}</div>
                 <div class="expense-item__description">
-                <h2>{ExpenseItem}</h2>
-                <h2>{LocationOfExpenditure}</h2>
-                <div className="expense-item__price">${ExpensePrice}</div>
+                <h2>{props.title}</h2>
+                <h2>{props.location}</h2>
+                <div className="expense-item__price">${props.amount}</div>
                 </div>
             </div>
        </div>
@@ -20,18 +16,3 @@ function ExpenseItems(){
 }
 
 export default ExpenseItems
-
-/*
-1. Component tree is similar to as DOM tree, a component inside the component is known an tree.
-2. App component will be the head of the component tree.
-3. Inorder to execute the code inside the ExpenseItem file, we need to export the file and import it in the App file.
-4. Custom made components are declared with uppercase of first letter, whereas normal HTML elements starts with lowercase letters.
-*/
-
-/*
-Why do we use className and not class?
-->It is an attribute/property of an element in React which is same as class. 
-
-1. How can we use Javascript variables in JS?
---> we can use the variables by using { } braces, by calling the variables inside the braces. 
-*/
