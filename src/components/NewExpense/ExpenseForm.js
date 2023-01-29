f21be411032e18940d73711442aa19917fd57023
karthik-140 <shanigaramkarthik@gmail.com>
2023-01-29 12:46:02 +0530

    removing-input-fields-from-UI

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -24,6 +24,9 @@ const ExpenseForm = () =>{
             date:new Date(enteredDate),
         }
         console.log(expenseData);
+        setEnteredTitle('');
+        setEnteredAmount('');
+        setEnteredDate('');
     }
    
     return (
@@ -31,15 +34,15 @@ const ExpenseForm = () =>{
             <div className='new-expense__controls'>
             <div className='new-expense__control'>
                 <label>Title</label>
-                <input type='text' onChange={titleChangeHandler} />
+                <input type='text' value={enteredTitle} onChange={titleChangeHandler} />
             </div>
             <div className='new-expense__control'>
                 <label>Amount</label>
-                <input type='number' min='0.01' step='0.01' onChange={amountChangeHandler} />
+                <input type='number' min='0.01' step='0.01' value={enteredAmount} onChange={amountChangeHandler} />
             </div>
             <div className='new-expense__control'>
                 <label>Date</label>
-                <input type='date' min='2019-01-14' step='2023-06-14' onChange={dateChangeHandler} />
+                <input type='date' min='2019-01-14' step='2023-06-14' value={enteredDate} onChange={dateChangeHandler} />
             </div>
             </div>
             <div>
