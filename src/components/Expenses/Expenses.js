8b2124c0dee676b7d1ea2a13138e85aab5480ebd
karthik-140 <shanigaramkarthik@gmail.com>
2023-01-29 15:37:00 +0530

    adding-filter-componenet

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,10 +1,19 @@
+import React, {useState} from 'react';
+
 import ExpenseItems from "./ExpenseItems";
 import './Expenses.css';
 import Card from '../UI/Card';
+import ExpensesFilter from "./ExpensesFilter";
+
 const Expenses = (props) => {
+    const [filteredYear,setFilteredYear]  = useState('2020');
+  const filterChangeHandler = selectedYear =>{
+       setFilteredYear(selectedYear);
+  }
    
       return (
         <Card className="expenses">
+          <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
            {props.items.map(data => <ExpenseItems 
             key={data.id}
             title={data.title}
