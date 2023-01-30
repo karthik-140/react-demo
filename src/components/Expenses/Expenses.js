979b8ae98e26d72ede3a710d4582b32571fb80a5
karthik-140 <shanigaramkarthik@gmail.com>
2023-01-30 11:40:31 +0530

    questions-answers-on-keys -&-map

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -27,3 +27,31 @@ const Expenses = (props) => {
 
 export default Expenses;
 
+
+
+ /* 1 -> Why did the Udemy trainer even convert the code.
+  Why doesn't he use ExpenseItem Component -> 4 times for showing 4 expenses. 
+  What do you think is the reason?
+
+1 --> Because it is a time taking process to enter each expense item manually, 
+we use an array which stores all the previous expenses and 
+concat the new expense at very when new expense is added using map() method. */
+
+/*Why does the trainer uses state and not update the old expense variable. 
+If you update the old expense variable like this expenses.push(expense). Why does this not work?
+
+---> Because in React it would not work like that, 
+in order to re-Execute the component we need to use State, which re-render the component again. */
+
+/*
+What happens when you don't use keys ?
+--> when we dont use keys, whenever we add expense the old expense state will be overwritten with the new expense state.
+
+What is the advantage of using keys?
+--> when we use keys, React identifies the key as unique which does not overwrite/change the state of the Expenses.
+It keeps the state of individual expenses of its own.
+
+Why you should not use index as keys?
+---> Index also create some bugs while adding expenses, because whenever we add new expense,
+it goes to the same index, so to overcome this, we use key which is a unique identifier.
+*/
